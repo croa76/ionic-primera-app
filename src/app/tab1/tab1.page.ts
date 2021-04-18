b1807c590b0a278cc07b19acc80a34d6f6f0633a
Cesar Olvera <croa76@hotmail.com>
2021-04-17 19:00:11 -0500

    Fin sección 2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit{
+
+  ngOnInit() {
+
+    let numero = 1;
+    debugger;
+    numero = 20;
+    numero = numero * 10;
+
+    console.log('helloword');
+
+  }
 
   constructor() {}
 
